65594337c195f4358a2c11a010cec1a12fd33249
syeero7 <EmalHasitha@proton.me>
2024-09-17 18:22:41 +0530

    Add a function to render the imported content

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,47 @@
-console.log("working");
+import homePage from "./home.js";
+import menuPage from "./menu.js";
+import contactPage from "./contact.js";
+import creditForImg from "./credit.js";
+import "../style.css";
+
+const content = document.querySelector("#content");
+
+(() => {
+  content.appendChild(homePage());
+})();
+
+document.querySelector("nav").addEventListener("click", generateContent);
+
+function renderContent(e) {
+  removeContent();
+
+  const target = e.target;
+  let pageContent;
+  switch (target.id) {
+    case "home":
+      pageContent = homePage;
+      break;
+    case "menu":
+      pageContent = menuPage;
+      break;
+    case "contact":
+      pageContent = contactPage;
+      break;
+  }
+  content.appendChild(pageContent());
+}
+
+function removeContent() {
+  while (content.firstChild) {
+    content.firstChild.remove();
+  }
+}
+
+(() => {
+  const footer = document.createElement("footer");
+  const credit = document.createElement("p");
+  credit.textContent = "Background ";
+  credit.appendChild(creditForImg("background"));
+  footer.appendChild(credit);
+  content.insertAdjacentElement("afterend", footer);
+})();
